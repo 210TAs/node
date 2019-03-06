7fc595f794c9759963477f0c75b6a3610bdb9e25
Chance Murray <chance.s.murray@gmail.com>
2019-03-06 16:11:39 -0700

    Update server.js

--- server.js
+++ server.js
@@ -209,8 +209,9 @@ app.get('/meme-hook', function (req, res){
 })
 // Tell the Browser to stop asking for the favicon :)
 app.get('/favicon.ico', function(req, res) {
-    res.status(204);
+    res.sendStatus(204);
 });
 app.all('*', function (req, res){
 	console.log('URL HIT WITH NO ROUTE', req.originalUrl)
+	res.sendStatus(404);
 })
